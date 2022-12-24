271e8756f7533fe7d94881ac9ba06eb24a4496c1
himself65 <himself65@outlook.com>
2022-12-25 06:33:46 +0800

    feat: init repo

--- next.config.js
+++ next.config.js
@@ -1,7 +1,21 @@
+const path = require('node:path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
 
-module.exports = nextConfig
+const baseDir = process.env.LOCAL_BLOCK_SUITE
+const withDebugLocal = require('next-debug-local')(
+  {
+    '@blocksuite/editor': path.resolve(baseDir, 'packages', 'editor'),
+    '@blocksuite/blocks': path.resolve(baseDir, 'packages', 'blocks'),
+    '@blocksuite/store': path.resolve(baseDir, 'packages', 'store'),
+  },
+  {
+    enable: process.env.LOCAL_BLOCK_SUITE !== '',
+  }
+)
+
+module.exports = withDebugLocal(nextConfig)
